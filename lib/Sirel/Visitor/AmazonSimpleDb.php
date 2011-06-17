24c1ae7d52dea2e7de5eb31b1f7b2e75abf78c22
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2011-06-17 08:23:48 +0200

    Added skeleton for AmazonSimpleDb Visitor

---
+++ lib/Sirel/Visitor/AmazonSimpleDb.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Sirel\Visitor;
+
+use Sirel\Node;
+
+/**
+ * Transforms the Query into SQL valid for Amazon Simple Db Select Queries
+ */
+class AmazonSimpleDb extends ToSql
+{
+}
